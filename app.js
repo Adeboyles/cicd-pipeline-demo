08249adbac45af20228c00bd3d17b54c48fe7e65
Adeboyles <oluwoleadeboye3@gmail.com>
2026-04-21 10:33:41 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+    res.json({
+        message: 'Hello, from Boyles! CI/CD Pipeline is live',
+        version: process.env.APP_VERSION || '1.0.0'
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
